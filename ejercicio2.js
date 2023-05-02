359a508f0e699b2ae43ecc9d28c8ae883762cebc
tomas alonso <alonsotomasgabriel@hotmail.com>
2023-05-02 15:08:45 -0300

    ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,11 @@
+const nombre="Tomas Alonso";
+const edad=22;
+const desarrollador=true;
+const nacimiento= new Date("2001-5-15");
+const libro={
+    titulo: "La insoportable levedad del ser",
+    autor: "Milan Kundera",
+    fecha: new Date("1984"),
+    url: ""
+}
+
