e969f81db2ab38164ac0fdb6db197bdb465ec8d5
tomas alonso <alonsotomasgabriel@hotmail.com>
2023-05-02 15:21:21 -0300

    ejercicio 3

---
+++ factorial-break.js
@@ -0,0 +1,10 @@
+let n = 10;
+let fact = 1;
+
+while (true) {
+    if (n == 0) {
+        break;
+    }
+    fact *= n;
+    n--;
+}
---
+++ factorial-while.js
@@ -0,0 +1,7 @@
+let factorial = 1;
+let i = 2;
+
+while (i <= 10) {
+    factorial *= i;
+    i++;
+}
