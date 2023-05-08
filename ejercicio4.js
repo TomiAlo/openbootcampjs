5661de24db72abff98b0248e62e0e7a7acdbf362
tomas alonso <alonsotomasgabriel@hotmail.com>
2023-05-08 12:34:37 -0300

    ejercicio 4

---
+++ ejercicio4.js
@@ -0,0 +1,7 @@
+const alturaCM = 176;
+const alturaM = 1.76;
+const pesoKG = 74.3;
+const alturaMRoundArriba = Math.ceil(alturaM);
+const pesoKGRoundAbajo = Math.floor(pesoKG);
+const esIgual = Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER;
+console.log(esIgual)
